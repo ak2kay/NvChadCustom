88944172d83084b6aba5afe69c6ecf037849a0d2
ak2kay <yandli@hotmail.com>
2023-04-20 20:19:55 +0800

    chore: port default neovim setting from old config

--- init.lua
+++ init.lua
@@ -1,7 +1,31 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+-- try to fix nvim overlapping in tmux
+vim.cmd [[
+  if &term =~ 'tmux'
+      set t_ut=y
+  endif
+]]
+
+-- better diff view, see: https://github.com/neovim/neovim/pull/14537
+vim.cmd [[set diffopt+=linematch:50]]
+
+
+local opt = vim.opt
+
+-- no swap file warning!
+opt.swapfile = false
+
+opt.joinspaces = false -- No double spaces with join after a dot
+
+opt.wildignorecase = true
+opt.wildignore:append "**/node_modules/*"
+opt.wildignore:append "**/.git/*"
+
+opt.relativenumber = true --Make relative number default
