e34133edce1edfee726cd6dc2d69342cbd22fcd2
dahufa <yandli@hotmail.com>
2023-04-22 16:26:20 +0800

    chore(mapping): port useful custom keymap from old configs

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,22 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["ggg"] = { "gg", "go to first line", opts = { nowait = true } },
+
+    ["Y"] = { "y$", "yank to the end of line" },
+
+    -- center search
+    ["n"] = { "nzzzv", "next search result" },
+    ["N"] = { "Nzzzv", "previous search result" },
+
+    -- pase last yanked, not deleted
+    ["<leader>p"] = { '"0p', "paste last yanked text below" },
+    ["<leader>P"] = { '"0P', "paste last yanked text above" },
+  },
+  v = {
+    ["J"] = { ":m '>+1<CR>gv=gv", "move selected lines down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", "move selected lines up" },
+    ["<"] = { "<gv", "shift selected lines left" },
+    [">"] = { ">gv", "shift selected lines right" },
   },
 }
 
