d31a298c70a61f34d98fb3af2faeb120c59ae9e6
ak2kay <yandli@hotmail.com>
2023-10-10 09:18:15 +0000

    feat(plugin): ajust harpoon popup window size

--- plugins.lua
+++ plugins.lua
@@ -253,6 +253,13 @@ local plugins = {
     end,
     dependencies = "nvim-lua/plenary.nvim",
     event = { "BufRead", "BufNewFile" },
+    config = function()
+      require("harpoon").setup {
+        menu = {
+          width = vim.api.nvim_win_get_width(0) / 2,
+        },
+      }
+    end,
   },
 
   -- markdown preview
